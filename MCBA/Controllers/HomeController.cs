5720a58a1703740abacd305b851f0de0e1e7a9a3
Russell0014 <Russells0014@gmail.com>
2025-09-17 23:43:38 +1000

    Removed unused using directives from controllers and models

--- MCBA/Controllers/HomeController.cs
+++ MCBA/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MCBA.Models;
-using Microsoft.Extensions.Logging;
 
 namespace MCBA.Controllers;
 
--- MCBA/Program.cs
+++ MCBA/Program.cs
@@ -1,9 +1,5 @@
 using MCBA.Data;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
