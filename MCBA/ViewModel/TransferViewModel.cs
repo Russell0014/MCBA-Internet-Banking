a4cb2281a3342b17f206d5c8bc235e857d412f08
elza-sprudzans <s4001795@student.rmit.edu.au>
2025-09-28 17:18:45 +1000

    make dest account a number

--- MCBA/ViewModel/TransferViewModel.cs
+++ MCBA/ViewModel/TransferViewModel.cs
@@ -1,17 +1,4 @@
-<<<<<<< Updated upstream
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// namespace MCBA.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
-namespace MCBA.ViewModel;
 
-// // ViewModel for Transfer
-// public class TransferViewModel {
-//     public int AccountNumber { get; set; }
-//     public int DestAccountNumber { get; set; }
-//     public SelectList? Accounts { get; set; }
-//     public decimal Amount { get; set; }
-//     public string? Comment { get; set; } // optional comment
-=======
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
 namespace MCBA.ViewModel;
@@ -30,7 +17,6 @@ public class TransferViewModel {
     public decimal Amount { get; set; }
     [StringLength(30, ErrorMessage = "Comment cannot exceed 30 characters.")]
     public string? Comment { get; set; } // optional comment
->>>>>>> Stashed changes
     
 
     // in the viewmodel we have account number, a list of accounts, dest account, amount, and comment. These are what is seen on the website and captures from the user.
