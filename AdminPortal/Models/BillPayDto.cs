5716ed9a970157adcaf9423860e7099e7e12237f
Russell0014 <Russells0014@gmail.com>
2025-10-04 17:14:35 +1000

    added BillPayDto and PayeeDto models with validation attributes

---
+++ AdminPortal/Models/BillPayDto.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminPortal.Models;
+
+public class BillPayDto
+{
+    public enum PeriodType
+    {
+        [Display(Name = "One Off")] OneOff = 1,
+        Monthly = 2
+    }
+
+    public enum StatusType
+    {
+        Completed = 1,
+        Pending = 2,
+        Failed = 3
+    }
+
+    public int BillPayId { get; set; }
+
+    public int AccountNumber { get; set; }
+
+    public int PayeeId { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Must be a positive value.")]
+    public decimal Amount { get; set; }
+
+    [Display(Name = "Schedule Time"), DataType(DataType.DateTime)]
+    public DateTime ScheduleTimeUtc { get; set; }
+
+    public PeriodType Period { get; set; }
+
+    public StatusType Status { get; set; }
+}
---
+++ AdminPortal/Models/PayeeDto.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminPortal.Models;
+
+public class PayeeDto
+{
+    public int PayeeId { get; set; }
+
+    [StringLength(50)] 
+    public string Name { get; set; }
+
+    [StringLength(50)] 
+    public string Address { get; set; }
+
+    [StringLength(40)] 
+    public string City { get; set; }
+
+    [StringLength(3)]
+    [RegularExpression(@"(?i)^(ACT|NSW|NT|QLD|SA|TAS|VIC|WA)$",
+        ErrorMessage = "Must be a valid 2 or 3 letter Australian state code.")]
+    public string State { get; set; }
+
+    [StringLength(4)]
+    [RegularExpression(@"^\d{4}$", ErrorMessage = "Must be exactly 4 digits.")]
+    public string Postcode { get; set; }
+
+    [StringLength(14)]
+    [RegularExpression(@"^\(0\d\) \d{4} \d{4}$",
+        ErrorMessage = "Must be of the format: (0X) XXXX XXXX")]
+    public string Phone { get; set; }
+}
